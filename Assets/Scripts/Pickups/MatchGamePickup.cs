1e6a18e090cdda416d8fc9c398742033d3b5899d
Bakkert2 <bakkert2@msu.edu>
2024-10-15 10:54:52 -0400

    Over-riding changes from main

--- Assets/Scripts/Pickups/MatchGamePickup.cs
+++ Assets/Scripts/Pickups/MatchGamePickup.cs
@@ -12,7 +12,7 @@ public class PuzzlePickup : Pickup
 
     public PlayerController player; // plyer
 
-    public TMPro.TMP_Text dialogText; // Reference to hint dialog text
+    public HintText dialogText; // Reference to hint dialog text
 
     public int lowerAmount = 3; // Amount to lower the hit threshold
 
@@ -49,7 +49,6 @@ public class PuzzlePickup : Pickup
             cursorManager.Puzzle();
             matchSystem.Start();
             matchWirePopUp.SetActive(true);
-            dialogText.text = "Match the switches!";
             //Debug.Log("Player near button. Press 'E' to interact.");
             playerInRange = true; // Player is in range
         }
@@ -59,7 +58,6 @@ public class PuzzlePickup : Pickup
     {
         if (other.CompareTag("Player") && !matchSystem.AllPaired())
         {
-            dialogText.text = "Something awesome!";
             playerInRange = false; // Player left the trigger area
         }
     }
@@ -67,13 +65,16 @@ public class PuzzlePickup : Pickup
     void ActivatePuzzle()
     {
 
-        dialogText.text = "Door almost opened. Hit it!";
+        
         Debug.Log("Door opened!");
 
         // Lower the hit threshold of the wall
         if (wallBreaker != null)
         {
-            wallBreaker.LowerHitThreshold(lowerAmount);
+            if (wallBreaker.LowerHitThreshold(lowerAmount))
+                dialogText.enterText = "Door opened. Let spread !";
+            else
+                dialogText.enterText = "Door almost opened. Hit it!";
         }
     }
 }
